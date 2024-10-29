11c741f2c4078b5f68aa4e7949b865d41449a1a9
Mauricio Bertei <1545592@sga.pucminas.br>
2024-10-28 21:16:23 -0300

    meutest a ver

--- ferramentas/02_selenium/test_exercise_solved.py
+++ ferramentas/02_selenium/test_exercise_solved.py
@@ -1,33 +1,60 @@
-import random
+import time
 import pathlib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ChromeOptions
 
+# Configurar o WebDriver para o Chrome no Windows
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
 
-def test_sample_page():
+def testa():
+    # localiza o HTML na mesma pasta do script usando pathlib
     file_path = pathlib.Path(__file__).parent.resolve()
-    driver = webdriver.Chrome()
+    driver.get(f"file:////{file_path}/sample-exercise_.html")
 
-    driver.get(f"file:///{file_path}/sample.html")
+    # localiza e clica no botão "generate"
+    driver.find_element(By.NAME, "generate").click()
 
-    title = driver.title
-    # assert title == "Web form"
-    assert title == "Sample page"
+    # espera que o código gerado possa ser visto e bota ele na variavel codigo
+    espera = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "my-value"))
+    )
+    codigo = espera.text
 
-    driver.implicitly_wait(0.5)
+    # coloca no campo de texto o código capturado
+    campo_texto = driver.find_element(By.ID, "input")
+    campo_texto.clear()
+    campo_texto.send_keys(codigo)
 
-    # text_box = driver.find_element(by=By.NAME, value="input")
-    text_box = driver.find_element(by=By.ID, value="input")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    # clica no botão "test"
+    driver.find_element(By.NAME, "button").click()
 
-    text = ["cheese", "selenium", "test", "bla", "foo"]
-    text_box.send_keys(text[random.randrange(len(text))])
-    from_input = text_box.get_attribute("value")
-    submit_button.click()
+    # fecha o alerta done
+    alerta = Alert(driver)
+    alerta.accept()
 
-    message = driver.find_element(by=By.ID, value="result")
-    value = message.text
-    # assert value == "It workls! Selenium!"
-    assert value == f"It works! {from_input}!"
+    # verifica a mensagem exibida
+    mensagem = driver.find_element(By.ID, "result").text
+    mensagem_esperada = f"It works! {codigo}!"
 
-    driver.quit()
+    #valida se teste passou ou falhou
+    if mensagem == mensagem_esperada: 
+        print("Teste OK ✔!")
+    else:
+        print("Teste falhou ❌!")
+
+# executa o teste 3x por pedido do professor
+for a in range(3):
+    testa()
+    time.sleep(3)  
+
+# fecha o chrome
+driver.quit()
